c300ab113cec004162e2c853b622b27d47a31dfe
Muhammad Talib Waseem <talibwaseem135@gmail.com>
2020-03-10 22:31:55 +0500

    weather api created

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const path=require('path')
 const express=require('express')
 const hbs = require('hbs')
-
+const geocode = require('./utils/geocode')
+const forecast =require('./utils/forecast')
 const app= express();
 
 
@@ -41,10 +42,28 @@ app.get('',(req, res) => {
 })
 
 app.get('/weather',(req,res)=>{
-    res.send({
-        forecast:'It is Raining',
-        location:'Karachi'
+    if(!req.query.address){
+        return res.send({
+            error:'You must provide an address'
+        })
+    }
+ 
+    geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+        if(error){
+            return res.send({error})
+        }
+        forecast(latitude,longitude,location,(error,forecastData)=>{
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forecast:forecastData,
+                location,
+                address:req.query.address
+            })
+        })
     })
+
 }) 
 
 app.get('/help/*',(req,res)=>{
@@ -53,6 +72,17 @@ app.get('/help/*',(req,res)=>{
         errorMessage:"Help Article Not Found",
         name:"Talib Waseem"
      })
+})
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+        res.send({
+            error:'You must provide a search term'  
+        })
+    }
+
+    res.send({
+        products:[]
+    })
 })
 
 app.get('*',(req,res)=>{
